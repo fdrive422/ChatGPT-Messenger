28b0a2d40f98e56c2ff67c84fbc02ed574783681
fdrive422 <fafrago@gmail.com>
2023-03-04 11:47:03 -0800

    start main page content, layout and add heroicons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import { SunIcon } from "@heroicons/react/24/outline";
+
 function Homepage() {
 	return (
-		<div>
-			<h1>ChatGPT Messenger</h1>
+		<div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+			<h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+			<div>
+				<div>
+					<div className="flex flex-col items-center mb-5">
+						{/* Sun Icon */}
+						<SunIcon className="h-8 w-8" />
+						<h2>Examples</h2>
+					</div>
+					<div className="space-y-2">
+						<p className="infoText">"Explain Something to me"</p>
+						<p className="infoText">
+							"What is the difference between a dog and a cat?"
+						</p>
+						<p className="infoText">
+							"What is the color of the sun?"
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
