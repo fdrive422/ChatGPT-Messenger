ab6d54f2db60b05fac6ebb98f2fb97a919c610fb
fdrive422 <fafrago@gmail.com>
2023-03-05 18:55:36 -0800

    integreate firebase databse, creat ChatRows to SideBae

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,19 +1,32 @@
 "use client";
 
+import { collection } from "firebase/firestore";
 import { useSession, signOut } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import ChatRow from "./ChatRow";
 import NewChat from "./NewChat";
 
 function SideBar() {
 	const { data: session } = useSession();
 
+	const [chats, loading, error] = useCollection(
+		session && collection(db, "users", session.user?.email!, "chats")
+	);
+
+	console.log(chats);
 	return (
 		<div className="flex flex-col h-screen p-2">
 			<div className="flex-1">
 				<div>
 					<NewChat />
+
 					<div>{/* ModelSelection */}</div>
 
 					{/* Map through the ChatRows */}
+					{chats?.docs.map((chat) => (
+						<ChatRow key={chat.id} id={chat.id} />
+					))}
 				</div>
 			</div>
 
